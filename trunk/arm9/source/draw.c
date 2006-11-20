98c3dac197da752e9f432537afdc52e69bd6b8ad
jjsimpso <jjsimpso@d0f03df7-d419-0410-85e3-47ab0de77107>
2006-11-20 05:20:36 +0000

    drawTitle() now works correctly.  We have graphics!

--- trunk/arm9/source/draw.c
+++ trunk/arm9/source/draw.c
@@ -13,15 +13,23 @@
 #include "globals.h"
 
 
-void bitBlt8(Uint8 *dest, Uint8 *src, int size){
-  //memcpy(dest, src, size);
-  memset(dest, 13, 1);
+void bitBlt8(Uint8 *dest, Uint8 *src, int w, int h){
+  int i;
+  Uint8 *line;
+
+  line = src;
+  for(i = 0; i < h; i++){
+    memcpy(dest, line, w);
+    dest += 512;
+    line += w;
+  }
+
 }
 
 void drawTitle(){
-  C4Img *title;
+  C8Img *title;
 
-  title = loadC4Img(gfxData, gfxndx, 3);
-  //title = loadC8Img(gfxData, gfxndx, 175);
-  bitBlt8((Uint8 *)BG_GFX, title->pixels, title->w * title->h);
+  //title = loadC4Img(gfxData, gfxndx, 3);
+  title = loadC8Img(gfxData, gfxndx, 175);
+  bitBlt8((Uint8 *)BG_GFX, title->pixels, title->w,  title->h);
 }
